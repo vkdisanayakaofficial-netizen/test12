488ec27f176b8dc39d4459e7af747f5138c202e3
vkdisanayakaofficial-netizen <vkdisanayakaofficial@gmail.com>
2025-12-01 23:07:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/vkdisanayakaofficial-netizen/test12/blob/main/images/vishu4.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VISHU-BoT Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94789651586',  // Replace with the owner's phone number
 
 
 
